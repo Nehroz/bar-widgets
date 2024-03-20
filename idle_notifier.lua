de8f67f43271216f5a58f2e21a3e6c9f2252db37
Nehroz <52700164+Nehroz@users.noreply.github.com>
2024-03-20 13:45:20 +0100

    Added Version 1.0 of two scripts.
    Added the nearest constructor script and idle (constructor) notifier script.

---
+++ idle_notifier.lua
@@ -0,0 +1,61 @@
+function widget:GetInfo()
+    return {
+        name = "Idle Constructor Notifiaction",
+        desc = "Audio queue and ping on idle cons appearing. alt+a move camera to latest idle and selects it until it's no longer idling; This will keep selecting idles in the same order as they appeared.",
+        author = "Nehroz",
+        date = "2024.3.x",
+        license = "GPL v3",
+        layer = 0,    
+        enabled = true,
+        version = "1.0"  --I may later add functioanlity to the option menu.
+    }
+end
+
+-- configurables
+local audio_queue = "Sounds/movement/arm-bot-at-sel.wav"  -- sound player with ping, to differtiate audio queue.
+local exlude_names = {"armcom", "corcom", "armrectr", "cornecro", "cormando"} -- excluded units that count as mobile builders.
+local interval_to_check = 30 -- how many game ticks for a check for idle removal.
+
+local is_play = false
+local idles = {}
+
+function widget:Initialize()
+    for _, v in pairs(exlude_names) do exlude_names[v] = true end -- setup for check
+    widget:Update()
+    Spring.Echo("Idle notificaiton loaded.")
+end
+
+function widget:Update()
+    is_play = Spring.GetSpectatingState()
+end
+
+function widget:UnitIdle(uID, uDefID, uClan)
+    if is_play ~= false then return end
+    if Spring.GetUnitIsDead(uID) ~= false then return end --supress idle detection on dead units.
+    if uClan ~= Spring.GetMyTeamID() then return end -- check if it's your unit before further checks.
+    def = UnitDefs[uDefID]
+    if def.isMobileBuilder == false or exlude_names[def.name] == true then return end -- mobile builder + exlude units
+    Spring.PlaySoundFile(audio_queue, 0.75, 'ui')
+    x, y, z = Spring.GetUnitPosition(uID)
+    Spring.MarkerAddPoint(x, y, z, "Idle ".. def.translatedHumanName .. "!" , true)
+    unit = {["x"] = x, ["y"] = y, ["z"] = z, ["uID"] = uID, ["uDefID"] = uDefID}
+    table.insert(idles, unit)
+end
+
+function widget:KeyPress(key, mods, isRepeating)
+    if key == 97 and mods.alt then -- a+alt
+        if #idles < 1 then return end
+        idle = idles[#idles]
+        if Spring.GetUnitIsDead(idle["uID"]) ~= false then return end
+        Spring.SetCameraTarget(idle["x"], idle["y"], idle["z"], 0.5)
+        Spring.SelectUnit(idle["uID"])
+        Spring.MarkerErasePosition(idle["x"], idle["y"], idle["z"])
+end end
+
+function widget:GameFrame(tick)
+    if math.fmod(tick, interval_to_check) == 0 then 
+        for i=#idles,1,-1 do --checks if idle no longer are idle, removes element and marker
+            if Spring.GetUnitCommands(idles[i]["uID"], 0) > 0 then
+                Spring.MarkerErasePosition(idles[i]["x"], idles[i]["y"], idles[i]["z"])
+                table.remove(idles, i)
+end end end end
---
+++ nearest_con.lua
@@ -0,0 +1,88 @@
+function widget:GetInfo()
+    return {
+        name = "Select nearest Constructor of type",
+        desc = "Selects nearest constructor on map, near mouse. T1 alt+q or T2 alt+w or alt+e to sellect all con's on map.",
+        author = "Nehroz",
+        date = "2024.3.x",
+        license = "GPL v3",
+        layer = 0,    
+        enabled = true,
+        version = "1.0" --I may later add functioanlity to the option menu.
+    }
+end
+
+local exlude_names = {"armcom", "corcom", "armrectr", "cornecro", "cormando", "corfast", "armfark", "armconsul"} -- units on this list are ignored by the script.
+local arm_t1_names = {'armck', 'armcv', 'armch', 'armca', 'armcs', 'armcsa', 'armbeaver'} -- Armada T1 constuctor names
+local cor_t1_names = {'corck', 'corcv', 'corch', 'corca', 'corcs', 'corcsa', 'cormuskrat'} -- Cortex T1 constructor names
+local arm_t2_names = {'armack', 'armacv', 'armaca', 'armacsub'} -- Armada T2
+local cor_t2_names = {'corack', 'coracv', 'coraca', 'coracsub'} -- Cortex T2
+local t1_names = {} -- init populated lists
+local t2_names = {}
+
+function vec_len(x,y,z) return math.sqrt(x*x+y*y+z*z) end -- simple vector math
+function make_set(tab) for _, key in ipairs(tab) do tab[key] = true end end -- coverts a table to a Set()-like
+function union(t1,t2) -- unifies two table into one.
+    new = {}
+    for i=1,#t1 do new[i] = t1[i] end
+    offset = #new
+    for i=1,#t2 do new[i+offset] = t2[i] end
+    return new
+end
+
+function get_all_cons() -- Returns table of all constructors qualifing.
+    us = Spring.GetTeamUnits(Spring.GetMyTeamID())
+    local cons = {}
+    if us == nil then return end
+    for _, uID in ipairs(us) do
+        def = UnitDefs[Spring.GetUnitDefID(uID)]
+        if def.isMobileBuilder == true then
+            if exlude_names[def.name] ~= true then
+                table.insert(cons, uID)
+    end end end
+    return cons
+end
+
+function get_mouse_pos()
+    x, y = Spring.GetMouseState()
+    _, args = Spring.TraceScreenRay(x,y, true)
+    if args == nil then return nil, nil, nil end
+    return args[1], args[2], args[3]
+end
+
+function find_nearest(t,kind) -- Takes table of all counstructors and a kind table that will act as filter. Returns the nearest unit of kind, nil if none.
+    m_x, m_y, m_z = get_mouse_pos()
+    if m_x == nil then return end -- break if out of map
+    distance = math.huge
+    nearest_uID = nil
+    for _, uID in pairs(t) do
+        if kind[UnitDefs[Spring.GetUnitDefID(uID)].name] == true then
+            x, y, z = Spring.GetUnitPosition(uID)
+            l = vec_len(x-m_x,y-m_y, z-m_z)
+            if l < distance then
+                distance = l
+                nearest_uID = uID
+    end end end
+    return nearest_uID
+end
+
+
+function widget:Initialize()
+    t1_names = union(arm_t1_names, cor_t1_names)
+    t2_names = union(arm_t2_names, cor_t2_names)
+    make_set(exlude_names)
+    make_set(t1_names)
+    make_set(t2_names)
+end
+
+function widget:KeyPress(key, mods, isRepeating)
+    if key == 101 and mods.alt then --e+alt
+        cons = get_all_cons()
+        if cons ~= nil then Spring.SelectUnitArray(cons) end
+    elseif key ==  113 and mods.alt then --q+alt
+        con = find_nearest(get_all_cons(), t1_names)
+        if con ~= nil then Spring.SelectUnit(con) end
+    elseif key == 119 and mods.alt then --w+alt
+        con = find_nearest(get_all_cons(), t2_names)
+        if con ~= nil then Spring.SelectUnit(con) end
+    end
+end
